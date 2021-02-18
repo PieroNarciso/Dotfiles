4e03321cc9c7cebc64ea1784cb558d9c8ef44f61
Piero Narciso <pieronarciso988@gmail.com>
2021-02-18 13:05:22 -0500

    Trash integration | syntax

--- .config/lf/scripts/preview.sh
+++ .config/lf/scripts/preview.sh
@@ -7,5 +7,6 @@ case "$1" in
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
     *.docx) docx2txt "$1" -;;
-    *) highlight -O ansi "$1" || cat "$1";;
+    *) bat --color always --theme gruvbox -p "$1" || cat "$1";;
+    # *) highlight -O ansi "$1" || cat "$1";;
 esac
