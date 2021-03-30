c3f4368d433e708b3500c9c4f185d810cc5e6814
Piero Narciso <pieronarciso988@gmail.com>
2021-03-30 11:05:40 -0500

    Modifed to POSIX (dash)

--- .scripts/volume_status_notify.sh
+++ .scripts/volume_status_notify.sh
@@ -6,7 +6,7 @@ icon_audio_muted="notification-audio-volume-muted"
 volume="$(amixer get Master | tail -1 | awk '{print $5}' | sed 's/[^0-9]*//g')"
 mute="$(amixer get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
 
-if [[ $volume == 0 || "$mute" == "off" ]]; then
+if [ $volume = 0 ] || [ $mute = "off" ]; then
     # Show the sound muted notification
     dunstify -u low -i $icon_audio_muted -r "$msg_id" -t 2000 "Volume muted"
 else
