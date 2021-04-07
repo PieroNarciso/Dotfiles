ad3964db22fbe90d3131b7db124154f54617c0b5
Piero Narciso <pieronarciso988@gmail.com>
2021-04-06 22:15:19 -0500

    Migrate config to lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+require('keybinds')
+require('config')
+require('plugins')
+
+-- Plugins
+require('nv-treesitter')
+require('nv-fzf')
+require('nv-airline')
+require('nv-nerdtree')
---
+++ .config/nvim/lua/nv-nerdtree/init.lua
@@ -0,0 +1,4 @@
+-- Toggle
+vim.cmd('map <C-b> :NERDTreeToggle<CR>')
+-- Position Window
+vim.cmd('let g:NERDTreeWinPos = "right"')
