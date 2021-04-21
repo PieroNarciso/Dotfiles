313f951244849a2f0d91dd1099b8603b57f87b50
Piero Narciso <pieronarciso988@gmail.com>
2021-04-20 19:14:49 -0500

    Added gruvbox colorscheme

--- .config/nvim/lua/nv-airline/init.lua
+++ .config/nvim/lua/nv-airline/init.lua
@@ -1,6 +1,6 @@
 -- Set tabline status enable
 vim.cmd('let g:airline#extensions#tabline#enabled = 1')
 -- Set theme
-vim.cmd('let g:airline_theme = "owo"')
+vim.g.airline_theme = "gruvbox_material"
 -- Set powerline arrows
-vim.cmd('let g:airline_powerline_fonts = 1')
+vim.g.airline_powerline_fonts = 1
