51d49bcd02fcc785140576f2fd7e6acdfe0e32a5
Piero Narciso <pieronarciso988@gmail.com>
2021-10-14 17:27:05 -0500

    Update presence and tree plugins config

--- config/.config/nvim/lua/nv-presence/init.lua
+++ config/.config/nvim/lua/nv-presence/init.lua
@@ -1,18 +1,22 @@
-Presence = require("presence"):setup({
-  -- Update activity based on autocmd events (if `false`, map or manually execute `:lua Presence:update()`)
-  auto_update       = true,
-  -- Editing format string (either string or function(filename: string|nil, buffer: string): string)
-  editing_text      = "Editing %s",
-  -- Workspace format string (either string or function(git_project_name: string|nil, buffer: string): string)
-  workspace_text    = "Working on %s",
-  -- Text displayed when hovered over the Neovim image
-  neovim_image_text = "The One True Text Editor",
-  -- Main image display (either "neovim" or "file")
-  main_image        = "neovim",
-  -- Use your own Discord application client id (not recommended)
-  client_id         = "793271441293967371",
-  -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-  log_level         = nil,
-  -- Number of seconds to debounce TextChanged events (or calls to `:lua Presence:update(<buf>, true)`)
-  debounce_timeout  = 15,
+require("presence"):setup({
+  -- General Options
+  auto_update         = true,
+  neovim_image_text   = "The One True Text Editor",
+  main_image          = "neovim",
+  client_id           = "793271441293967371",
+  log_level           = nil,
+  debounce_timeout    = 10,
+  enable_line_number  = false,
+  blacklist           = {},
+  buttons             = true,
+
+  -- Rich Presence text options
+
+  editing_text        = "Editing %s",
+  file_explorer_text  = "Browsing %s",
+  git_commit_text     = "Committing changes",
+  plugin_manager_text = "Managing plugins",
+  reading_text        = "Reading %s",
+  workspace_text      = "Working on %s",
+  line_number_text    = "Line %s out of %s",
 })
