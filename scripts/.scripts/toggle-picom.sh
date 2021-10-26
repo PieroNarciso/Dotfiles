3502f14cc1dca7272e8c230fb21d8cf5d1619b61
Piero Narciso <pieronarciso988@gmail.com>
2021-10-26 16:40:47 -0500

    Added notification of compositor status

--- scripts/.scripts/toggle-picom.sh
+++ scripts/.scripts/toggle-picom.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+msg_id="8855754"
 
 if pgrep -x picom > /dev/null; then
     pkill picom &
+    dunstify -u low -r "$msg_id" -t 2000 "Compositor Disabled"
 else
     picom --experimental-backends &
+    dunstify -u low -r "$msg_id" -t 2000 "Compositor Enabled"
 fi
