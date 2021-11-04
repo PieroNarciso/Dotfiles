c089cf3b45a0c66bf40c80c3bf62d94474bcbcf0
Piero Narciso <pieronarciso988@gmail.com>
2021-11-03 20:52:32 -0500

    Change lspinstall plugin

--- config/.config/nvim/lua/lsp/vue.lua
+++ config/.config/nvim/lua/lsp/vue.lua
@@ -1,47 +1,4 @@
 return {
   filetypes = { "vue" },
-  init_options = {
-    config = {
-      css = {},
-      emmet = {},
-      html = {
-        suggest = {}
-      },
-      javascript = {
-        format = {}
-      },
-      stylusSupremacy = {},
-      typescript = {
-        format = {}
-      },
-      vetur = {
-        completion = {
-          autoImport = true,
-          tagCasing = "kebab",
-          useScaffoldSnippets = false
-        },
-        format = {
-          defaultFormatter = {
-            js = "none",
-            ts = "none"
-          },
-          defaultFormatterOptions = {},
-          scriptInitialIndent = false,
-          styleInitialIndent = false
-        },
-        experimental = {
-          templateInterpolationService = true
-        },
-        useWorkspaceDependencies = false,
-        validation = {
-          templateProps = true,
-          script = true,
-          style = true,
-          template = true,
-          interpolation = true,
-        }
-      }
-    }
-  },
   root_dir = require('lspconfig/util').root_pattern("package.json", "vue.config.js")
 }
