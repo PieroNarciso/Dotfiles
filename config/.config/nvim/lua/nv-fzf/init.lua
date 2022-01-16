917c7524806f1eabd3ccd6b9b32f066ccb2d7b26
Piero Narciso <pieronarciso988@gmail.com>
2022-01-16 13:10:37 -0500

    Nvim configuration files move to other repo

--- config/.config/nvim/lua/nv-fzf/init.lua
+++
@@ -1,5 +0,0 @@
-local set = vim.api.nvim_set_keymap
-
-set('n', '<C-p>', ':Files<CR>', { noremap = true })
-set('n', '<Leader>ps', ':Rg<CR>', { noremap = true })
-set('n', '<Leader>pb', ':Buffers<CR>', { noremap = true })
--- config/.config/nvim/lua/nv-nerdtree/init.lua
+++
@@ -1,5 +0,0 @@
-local map = vim.api.nvim_set_keymap
--- Toggle
-map('', '<C-b>', ':NERDTreeToggle<CR>', { silent = true })
--- Position Window
-vim.g.NERDTreeWinPos = "right"
